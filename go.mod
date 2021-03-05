8f1d8c8dd4f7d2cfbaaa6c8112bdf3b6d66907c6
Chiro <chiro2001@163.com>
2021-03-06 00:01:43 +0800

    网络部分√

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module magic_mouse
 
 go 1.15
+
+require github.com/gogf/gf v1.15.4 // indirect
--- magic_mouse.go
+++ magic_mouse.go
@@ -1,33 +1,63 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
 )
 
 var indexHTML []byte // = bytes.NewBuffer([]byte{})
 // 运行配置
 var configRefreshIndex bool = true
 
-func index(w http.ResponseWriter, r *http.Request) {
-	if len(indexHTML) == 0 || configRefreshIndex {
-		// 读取index
-		f, err := ioutil.ReadFile("./frontend/index.html")
-		if err != nil {
-			fmt.Println("read fail", err)
-		}
-		indexHTML = f
+type DataType struct {
+	Alpha   float32 `json:"alpha"`
+	Beta    float32 `json:"beta"`
+	Gamma   float32 `json:"gamma"`
+	Key     int     `json:"key"`
+	Message string  `json:"message"`
+}
+
+type Message struct {
+	Cmd  string   `json:"cmd"`
+	Data DataType `json:"data"`
+}
+
+func dropError(e error) {
+	if e != nil {
+		panic(e)
 	}
-	log.Printf("visit index")
-	w.Write(indexHTML)
 }
 
 func main() {
 	// 建立静态文件服务器
-	fs := http.FileServer(http.Dir("./frontend/"))
-	http.HandleFunc("/", index)
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.ListenAndServe(":14514", nil)
+	wp := g.Server("WebpageService")
+	wp.SetServerRoot("./frontend/")
+	wp.SetPort(80)
+	wp.Start()
+	// WebSocket 服务器
+	s := g.Server("WebSocketService")
+	s.BindHandler("/api/v1/ws", func(r *ghttp.Request) {
+		ws, err := r.WebSocket()
+		dropError(err)
+		for {
+			msgType, msg, err := ws.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// 在这里弹一首 **夏威夷吉他**！！
+			fmt.Println(msgType, string(msg), err)
+			var coordinate Message
+			json.Unmarshal(msg, &coordinate)
+			fmt.Println(coordinate)
+		}
+	})
+	s.SetFileServerEnabled(false)
+	s.SetPort(14514)
+	s.Start()
+
+	g.Wait()
 }
