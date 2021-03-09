24f11a7d123eac15817cb347f82595980f1a655e
Hans Wan <59825102+criwits@users.noreply.github.com>
2021-03-09 23:41:17 +0800

    目录更名。

--- src/main/move_mouse.go
+++ src/main/move_mouse.go
@@ -7,6 +7,6 @@ import (
 
 func moveMouse(oriAngle, curAngle Angle, screen Screen, distance float32) {
 	// Hawaii Guitar here!
-	robotgo.MoveMouse(screen.Width/2-int(float32(math.Tan(float64(curAngle.Alpha-oriAngle.Alpha) * math.Pi / 180))*distance),
-		screen.Height/2-int(float32(math.Tan(float64(curAngle.Beta-oriAngle.Beta) * math.Pi / 180))*distance))
+	robotgo.MoveMouse(screen.Width/2-int(float32(math.Tan(float64(curAngle.Alpha-oriAngle.Alpha)*math.Pi/180))*distance),
+		screen.Height/2-int(float32(math.Tan(float64(curAngle.Beta-oriAngle.Beta)*math.Pi/180))*distance))
 }
