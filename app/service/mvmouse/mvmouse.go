5ce0cdaecbbc3741aa6acc4c2e6930e660cf9e3e
Hans Wan <59825102+criwits@users.noreply.github.com>
2021-03-12 13:28:34 +0800

    加入一点说明语句，释放 v0.1 版本

--- app/service/mvmouse/mvmouse.go
+++ app/service/mvmouse/mvmouse.go
@@ -7,12 +7,13 @@ import (
 )
 
 func MoveMouse(oriAngle, curAngle mgmtype.Angle, screen mgmtype.Screen, distance float32) {
-	x, y := screen.Width/2-int(float32(math.Tan(float64(curAngle.Alpha-oriAngle.Alpha)*math.Pi/180))*distance),
-		screen.Height/2-int(float32(math.Tan(float64(curAngle.Beta-oriAngle.Beta)*math.Pi/180))*distance)
-	if x < 0 || x >= screen.Width || y < 0 || y >= screen.Height {
-		return
-	}
-	robotgo.MoveMouse(
+	/*	x, y := screen.Width/2-int(float32(math.Tan(float64(curAngle.Alpha-oriAngle.Alpha)*math.Pi/180))*distance),
+			screen.Height/2-int(float32(math.Tan(float64(curAngle.Beta-oriAngle.Beta)*math.Pi/180))*distance)
+		if x < 0 || x >= screen.Width || y < 0 || y >= screen.Height {
+			return
+		}*/
+	robotgo.MoveMouseSmooth(
 		screen.Width/2-int(float32(math.Tan(float64(curAngle.Alpha-oriAngle.Alpha)*math.Pi/180))*distance),
-		screen.Height/2-int(float32(math.Tan(float64(curAngle.Beta-oriAngle.Beta)*math.Pi/180))*distance))
+		screen.Height/2-int(float32(math.Tan(float64(curAngle.Beta-oriAngle.Beta)*math.Pi/180))*distance),
+		0.08, 0.10)
 }
--- boot/boot.go
+++ boot/boot.go
@@ -1,20 +1,11 @@
 package boot
 
 import (
-	"fmt"
 	"magic_mouse/app/model/mgmargs"
-	"magic_mouse/app/service/droperror"
-	"magic_mouse/app/service/ipget"
 	"math"
 )
 
 func init() {
-	fmt.Println("Your IP address(es):")
-	ips, err := ipget.GetLocalIP()
-	droperror.DropError(err)
-	for _, ip := range ips {
-		fmt.Println("\t", ip)
-	}
 	mgmargs.Screen = GetScreenMetrics()
 	mgmargs.Distance = float32(math.Sqrt(float64(mgmargs.Screen.Height*mgmargs.Screen.Height +
 		mgmargs.Screen.Width*mgmargs.Screen.Width)))
